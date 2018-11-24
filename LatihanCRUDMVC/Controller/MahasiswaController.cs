04667fd05893fbbf8cbe5245eda49670ff1cc457
Anas Setiawan <36320727+anasleet@users.noreply.github.com>
2018-11-24 08:44:04 +0700

    Add files via upload

---
+++ LatihanCRUDMVC/Controller/MahasiswaController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using LatihanCRUDMVC.Model.Entity;
+using LatihanCRUDMVC.Model.Repository;
+using LatihanCRUDMVC.Model.Context;
+
+namespace LatihanCRUDMVC.Controller
+{
+    public class MahasiswaController
+    {
+
+        // deklarasi objek _repository
+        private MahasiswaRepository _repository;
+        public int Save(Mahasiswa obj)
+        {
+            var result = 0;
+
+            if (string.IsNullOrEmpty(obj.npm))
+            {
+                MessageBox.Show("NPM harus di isi!!!");
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(obj.nama))
+            {
+                MessageBox.Show("Nama harus di isi!!!");
+                return 0;
+            }
+
+            using (var context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                result = _repository.Save(obj);
+            }
+            return result;
+        }
+
+        public int Update(Mahasiswa obj)
+        {
+            var result = 0;
+
+            if (string.IsNullOrEmpty(obj.npm))
+            {
+                MessageBox.Show("Npm harus di isi!!!");
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(obj.nama))
+            {
+                MessageBox.Show("Nama harus di isi!!!");
+                return 0;
+            }
+
+            using (var context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                result = _repository.Save(obj);
+            }
+            return result;
+
+        }
+
+        public int Delete(Mahasiswa obj)
+        {
+            var result = 0;
+
+            if (string.IsNullOrEmpty(obj.npm))
+            {
+                MessageBox.Show("NPM harus di isi!!!");
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(obj.nama))
+            {
+                MessageBox.Show("Nama harus di isi!!!");
+                return 0;
+            }
+
+            using (var context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                result = _repository.Save(obj);
+            }
+            return result;
+
+        }
+
+        public List<Mahasiswa> GetAll()
+        {
+            var list = new List<Mahasiswa>();
+
+            using (var context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                list = _repository.GetAll();
+
+            }
+
+            return list;
+        }
+    }
+}
---
+++ LatihanCRUDMVC/Model/Repository/MahasiswaRepository.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.OleDb;
+using LatihanCRUDMVC.Model.Context;
+using LatihanCRUDMVC.Model.Entity;
+
+namespace LatihanCRUDMVC.Model.Repository
+{
+    public class MahasiswaRepository
+    {
+        // deklarasi objek _context
+        private DbContext _context;
+
+        // constructor
+        public MahasiswaRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public int Save(Mahasiswa obj)
+        {
+            var result = 0;
+            var sql = @"insert into mahasiswa (npm, nama, angkatan) values (@npm, @nama, @angkatan)";
+            using (var cmd = new OleDbCommand(sql, _context.Conn))
+            {
+                cmd.Parameters.AddWithValue("@npm", obj.npm);
+                cmd.Parameters.AddWithValue("@nama", obj.nama);
+                cmd.Parameters.AddWithValue("@angkatan", obj.angkatan);
+
+                result = cmd.ExecuteNonQuery();
+            }
+
+            return result;
+        }
+
+        public int Update(Mahasiswa obj)
+        {
+            var result = 0;
+            var sql = @"update mahasiswa set nama = @nama, angkatan = @angkatan
+where npm = @npm";
+            using (var cmd = new OleDbCommand(sql, _context.Conn))
+            {
+                cmd.Parameters.AddWithValue("@npm", obj.npm);
+                cmd.Parameters.AddWithValue("@nama", obj.nama);
+                cmd.Parameters.AddWithValue("@angkatan", obj.angkatan);
+
+                result = cmd.ExecuteNonQuery();
+            }
+
+            return result;
+
+        }
+
+        public int Delete(Mahasiswa obj)
+        {
+            var result = 0;
+            var sql = @"DELETE FROM mahasiswa WHERE npm = @npm";
+            using (var cmd = new OleDbCommand(sql, _context.Conn))
+            {
+                cmd.Parameters.AddWithValue("@npm", obj.npm);
+
+                result = cmd.ExecuteNonQuery();
+            }
+
+            return result;
+
+        }
+
+        public List<Mahasiswa> GetAll()
+        {
+            var list = new List<Mahasiswa>();
+
+            var sql = @"select npm, nama, angkatan from mahasiswa order by nama";
+            using (var cmd = new OleDbCommand(sql, _context.Conn))
+            {
+                using (var dtr = cmd.ExecuteReader())
+                {
+                    while (dtr.Read())
+                    {
+                        // membuat objek 
+                        var mhs = new Mahasiswa();
+                        // mapping objek dara reader
+                        mhs.npm = dtr["npm"] == DBNull.Value ? string.Empty : dtr["npm"].ToString();
+                        mhs.nama = dtr["nama"] == DBNull.Value ? string.Empty : dtr["nama"].ToString();
+                        mhs.npm = dtr["angkatan"] == DBNull.Value ? string.Empty : dtr["angkatan"].ToString();
+
+                        // tambhakan objek ke dalam objek collection
+                        list.Add(mhs);
+
+                    }
+                }
+            }
+            return list;
+        }
+
+
+
+    }
+}
